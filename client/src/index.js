da637f2c1c2975d7a4cd506a3396101256ffadfa
Nader elhaj <naderelhaj3@gmail.com>
2021-02-16 01:07:36 +0100

    Front End Completd

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import {createStore,applyMiddleware,compose} from 'redux'
-import {useDispatch} from 'react-redux'
-import ReactDom from 'react-dom'
-import {Provider} from 'react-redux'
-import thunk from 'redux-thunk'
-import reducers from './reducers'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
-import App from './App'
+import { reducers } from './reducers';
+import App from './App';
+import './index.css';
 
-const store = createStore(reducers,compose(applyMiddleware(thunk)));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
-ReactDom.render(
-    <Provider store ={store}>
-        <App/>
-    </Provider>,
-document.getElementById("root")
-)
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
--- server/index.js
+++ server/index.js
@@ -11,10 +11,10 @@ const app = express();
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
+
 app.use('/posts', postRoutes);
 
-
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const CONNECTION_URL = 'mongodb+srv://memories:3171999@cluster0.isaho.mongodb.net/test?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
